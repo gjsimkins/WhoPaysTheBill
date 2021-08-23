f310e4522a02fb7396f9f65d864a3e961bc7dc0e
gjsimkins <baseball3gary@gmail.com>
2021-08-22 19:04:42 -0700

    Initial commit

---
+++ src/context/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const MyContext = React.createContext();
+
+class MyProvider extends Component {
+    state = {
+        stage: 1,
+        players: [],
+        result: ''
+    }
+
+    addPlayerHandler = (name) => {
+        this.setState((prevState) => ({
+            players: [...prevState.players, name]
+        }))
+    }
+
+    removePlayerHandler = (idx) => {
+        let newArray = this.state.players;
+        newArray.splice(idx, 1);
+        this.setState({ players: newArray });
+    }
+
+    generateLoser = () => {
+        const { players } = this.state;
+        this.setState({
+            result: players[Math.floor(Math.random() * players.length)]
+        })
+    }
+
+    nextHandler = () => {
+        const { players } = this.state
+
+        if (players.length < 2) {
+            toast.error("You need more than one player", {
+                position: toast.POSITION.TOP_LEFT,
+                autoClose: 2000
+            })
+        } else {
+            this.setState({ stage: 2 }, () => {
+                setTimeout(() => {
+                    this.generateLoser()
+                }, 2000)
+            })
+        }
+    }
+
+    resetGame = () => {
+        this.setState({
+            stage: 1,
+            players: [],
+            result: ''
+        })
+    }
+
+
+
+    render() {
+        return (
+            <>
+                <MyContext.Provider value={{
+                    state: this.state,
+                    addPlayer: this.addPlayerHandler,
+                    removePlayer: this.removePlayerHandler,
+                    next: this.nextHandler,
+                    generateLoser: this.generateLoser,
+                    resetGame: this.resetGame
+                }}>
+                    {this.props.children}
+                </MyContext.Provider>
+                <ToastContainer />
+            </>
+        )
+    }
+}
+
+export { MyContext, MyProvider }
